79e321d079c119a87bcec316ccf96137814229e5
Mannu Lambrichts <mannu346@gmail.com>
2025-08-28 12:30:27 +0100

    base path

--- svelte.config.js
+++ svelte.config.js
@@ -17,8 +17,9 @@ const config = {
 			}
 		),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+			relative: false
+		},
 	}
 };
 
